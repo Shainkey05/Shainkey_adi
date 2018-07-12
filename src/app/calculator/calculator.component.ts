e0cf58136859bd6e508d5c7ec153f2dd5126aca7
shainkey jain <shainkey.jain@habilelabs.io>
2018-07-12 14:38:36 +0530

    Add ngmodel in calculator component

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent implements OnInit {
+  public number1: number;
+  public number2: number;
+  public result: number;
+
+  public add() {
+    this.result = this.number1 + this.number2;
+  }
 
   constructor() { }
 
   ngOnInit() {
-  }
+    }
 
 }
+
